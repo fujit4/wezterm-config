0095d9c8c777f3c25480da3f28c4752d01fb76d9
fujit4 <fujitash4@gmail.com>
2025-08-16 21:30:56 +0900

    change color

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,8 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Gruvbox (Gogh)'
+-- config.color_scheme = 'Ivory Light (terminal.sexy)'
+config.color_scheme = 'Catppuccin Latte'
 config.font = wezterm.font 'PlemolJP HSNF'
 
 config.automatically_reload_config = true
